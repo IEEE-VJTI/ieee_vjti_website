0b67f7efaaa047661a0c2781021ea275f580e5f3
IEEE-VJTI <112966360+IEEE-VJTI@users.noreply.github.com>
2022-12-10 15:16:31 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://ieeevjtiserver.herokuapp.com" });
+const API = axios.create({ baseURL: "https://ieee-backend-seven.vercel.app" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
